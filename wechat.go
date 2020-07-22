c946836ec1d12e3e6d6bdcc207b1d3801f5dcc8f
fideism <nignog@126.com>
2020-07-22 12:42:37 +0800

    解决冲突

--- wechat.go
+++ wechat.go
@@ -1,14 +1,10 @@
 package wechat
 
 import (
-<<<<<<< HEAD
 	"os"
 
 	"github.com/fideism/golang-wechat/cache"
-=======
-	"github.com/fideism/golang-wechat/cache"
 	logger "github.com/fideism/golang-wechat/log"
->>>>>>> ab3c5799693c669ca46f2f759fc28f67a35e865c
 	"github.com/fideism/golang-wechat/miniprogram"
 	miniConfig "github.com/fideism/golang-wechat/miniprogram/config"
 	"github.com/fideism/golang-wechat/officialaccount"
@@ -18,21 +14,13 @@ import (
 	"github.com/fideism/golang-wechat/pay"
 	payConfig "github.com/fideism/golang-wechat/pay/config"
 	log "github.com/sirupsen/logrus"
-<<<<<<< HEAD
-=======
-	"os"
->>>>>>> ab3c5799693c669ca46f2f759fc28f67a35e865c
 )
 
 func init() {
 	// Log as JSON instead of the default ASCII formatter.
-<<<<<<< HEAD
-	log.SetFormatter(&log.TextFormatter{})
-=======
 	log.SetFormatter(&logger.LogstashFormatter{
 		Channel: "WeChat",
 	})
->>>>>>> ab3c5799693c669ca46f2f759fc28f67a35e865c
 
 	// Output to stdout instead of the default stderr
 	// Can be any io.Writer, see below for File example
@@ -53,36 +41,6 @@ func NewWechat() *Wechat {
 }
 
 //SetCache 设置cache
-<<<<<<< HEAD
-func (wc *Wechat) SetCache(cahce cache.Cache) {
-	wc.cache = cahce
-}
-
-//GetOfficialAccount 获取微信公众号实例
-func (wc *Wechat) GetOfficialAccount(cfg *offConfig.Config) *officialaccount.OfficialAccount {
-	if cfg.Cache == nil {
-		cfg.Cache = wc.cache
-	}
-	return officialaccount.NewOfficialAccount(cfg)
-}
-
-// GetMiniProgram 获取小程序的实例
-func (wc *Wechat) GetMiniProgram(cfg *miniConfig.Config) *miniprogram.MiniProgram {
-	if cfg.Cache == nil {
-		cfg.Cache = wc.cache
-	}
-	return miniprogram.NewMiniProgram(cfg)
-}
-
-// GetPay 获取微信支付的实例
-func (wc *Wechat) GetPay(cfg *payConfig.Config) *pay.Pay {
-	return pay.NewPay(cfg)
-}
-
-// GetOpenPlatform 获取微信开放平台的实例
-func (wc *Wechat) GetOpenPlatform(cfg *openConfig.Config) *openplatform.OpenPlatform {
-	return openplatform.NewOpenPlatform(cfg)
-=======
 func (w *Wechat) SetCache(cache cache.Cache) {
 	w.cache = cache
 }
@@ -111,5 +69,4 @@ func (w *Wechat) GetPay(c *payConfig.Config) *pay.Pay {
 // GetOpenPlatform 获取微信开放平台的实例
 func (w *Wechat) GetOpenPlatform(c *openConfig.Config) *openplatform.OpenPlatform {
 	return openplatform.NewOpenPlatform(c)
->>>>>>> ab3c5799693c669ca46f2f759fc28f67a35e865c
 }
